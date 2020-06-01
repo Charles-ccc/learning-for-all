50003ae6984fb6d0634e1a510b22e85e28204601
Charles-ccc <liu930505@qq.com>
2020-06-01 16:00:23 +0800

    myredux

--- react/react-cmpt/src/components/MyReduxTest.js
+++ react/react-cmpt/src/components/MyReduxTest.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { createStore } from './../store/kredux'
+import { createStore, applyMiddleware } from '../store/kredux'
 import { Button } from 'antd'
 
 const counterReducer = (state = 0, action) => {
@@ -15,7 +15,27 @@ const counterReducer = (state = 0, action) => {
   }
 }
 
-const store = createStore(counterReducer)
+// 自定义中间件
+const logger = ({dispatch, getState}) =>{
+  // 返回真正中间件任务执行函数
+  return dispatch => action => {
+    // 执行中间件任务
+    console.log(action.type + '执行了！！！')
+    // 执行下一个中间件
+    return dispatch(action)
+  }
+}
+
+const thunk = ({dispatch, getState}) => dispatch => action => {
+  // 处理函数action
+  if ( typeof action == 'function' ) {
+    return action(dispatch, getState)
+  }
+  // 不是函数直接跳过
+  return dispatch(action)
+}
+
+const store = createStore(counterReducer, applyMiddleware(logger, thunk))
 export default class MyReduxTest extends Component {
 
   componentDidMount () {
@@ -28,6 +48,11 @@ export default class MyReduxTest extends Component {
         <h3>这里是自建redux - {store.getState()}</h3>
         <br />
         <Button onClick={ () => store.dispatch( { type: 'add'} ) }>+</Button>
+        <Button onClick={ () => store.dispatch( () => {
+          setTimeout( () => {
+            store.dispatch({type: 'add'})
+          }, 1000)
+        }) }>+</Button>
       </div>
     )
   }
--- react/react-cmpt/src/store/kredux.js
+++ react/react-cmpt/src/store/kredux.js
@@ -1,5 +1,10 @@
+export function createStore (reducer, enhancer) {
+  // enhancer 为高阶函数，强化createStore的能力
+  // 返回函数执行reducer
+  if (enhancer) {
+    return enhancer(createStore)(reducer)
+  }
 
-export function createStore (reducer) {
   let currentState = undefined
   const currentListeners = [] // 订阅的所有回调函数数组
 
@@ -8,7 +13,7 @@ export function createStore (reducer) {
   }
 
   // 更新状态
-  function dispatch ( action ) {
+  function dispatch (action) {
     // 修改
     currentState = reducer(currentState, action)
     // 变更通知，执行所有回调函数
@@ -27,4 +32,43 @@ export function createStore (reducer) {
     dispatch,
     subsucribe
   }
+}
+
+// 接收若干中间件
+export function applyMiddleware ( ...middlewares ) {
+  // 从enhancer中接收的createStore
+  return createStore => (...args) => {
+    // 完成之前 createStore 的工作，args包含reducer
+    const store = createStore(...args)
+    let dispatch = store.dispatch
+    // 强化 dispatch
+    // 传递给中间件函数的参数
+    const midApi = {
+      getState: store.getState,
+      dispatch: (...args) => dispatch(...args)
+    }
+    // 将若干middlewares转为数组
+    const chain = middlewares.map(mw => mw(midApi))
+    // 将函数的数组聚合成一个多层级嵌套函数
+    // 把强化后的dispatch再赋值给它自己
+    // 按顺序执行中间件函数
+    dispatch = compose(...chain)(store.dispatch)
+    // 返回全新store，仅更新强化过的dispatch函数
+    return {
+      ...store,
+      dispatch
+    }
+  }
+}
+
+export function compose (...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+  // 聚合函数数组为一个函数 [fn1, fn2] => fn2(fn1())
+  // ...args = store.dispatch
+  return funcs.reduce((left, right) => (...args) => right(left(...args)))
 }
