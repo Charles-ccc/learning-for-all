b5a3f196b1673efba71d53b0ca2f02b3157b2fdd
Charles-ccc <liu930505@qq.com>
2020-08-06 23:32:20 +0800

    leetcode

--- learn-js/leetcode/242-有效的字母异位词.js
+++ learn-js/leetcode/242-有效的字母异位词.js
@@ -4,7 +4,16 @@
  * @return {boolean}
  */
 // 字母出现的次数相等的两个词，就是异位词
-var isAnagram = function(s, t) {
+const isAnagram = function(s, t) {
   // return s.split().sort().join() === t.split().sort().join()
-  
+  if (s.length !== t.length) return false
+  for (let char of s) {
+    if (t.indexOf(char) !== -1) {
+      const index = t.indexOf(char)
+      // remove index
+      t = t.slice(0, index) + t.slice(index + 1)
+    }
+  }
+  if (t) return false
+  return true
 };
