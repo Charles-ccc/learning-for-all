d35ca0137cf677527ae59e8b60982e50099cc767
Charles-ccc <liu930505@qq.com>
2020-08-30 22:29:02 +0800

    koa-jwt

--- koa-zhihu/app/routes/users.js
+++ koa-zhihu/app/routes/users.js
@@ -1,22 +1,24 @@
-const JsonWebToken = require('jsonwebtoken')
+// const JsonWebToken = require('jsonwebtoken')
+const JWT = require('koa-jwt')
 const Router = require('koa-router')
 const router = new Router({prefix: '/users'})
 const { find, findById, update, create, delete: del, login, checkOwner } = require('../controllers/users')
 const { secret } = require('../config.js')
 
 // 认证
-const auth = async (ctx, next) => {
-  const  { authorization = '' } = ctx.request.header
-  const token = authorization.replace('Bearer ', '')
-  try {
-    const user = JsonWebToken.verify(token, secret)
-    // state通常用来存放用户信息
-    ctx.state.user = user
-  } catch (err) {
-    ctx.throw(401, err.message)
-  }
-  await next()
-}
+const auth = JWT({ secret })
+// const auth = async (ctx, next) => {
+//   const  { authorization = '' } = ctx.request.header
+//   const token = authorization.replace('Bearer ', '')
+//   try {
+//     const user = JsonWebToken.verify(token, secret)
+//     // state通常用来存放用户信息
+//     ctx.state.user = user
+//   } catch (err) {
+//     ctx.throw(401, err.message)
+//   }
+//   await next()
+// }
 
 // 注入控制器
 router.get('/', find)
