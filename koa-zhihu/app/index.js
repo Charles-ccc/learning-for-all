600f0ab29df342fb76b4d496a9d93355670192af
Charles-ccc <liu930505@qq.com>
2020-08-31 23:29:49 +0800

    koa-body

--- koa-zhihu/app/index.js
+++ koa-zhihu/app/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
-const BodyParser = require('koa-bodyparser') // 解析请求体
+// const BodyParser = require('koa-bodyparser') // 解析请求体
+const KoaBody = require('koa-body')
 const Error = require('koa-json-error') // 错误处理中间件
 const Parameter = require('koa-parameter') // 参数校验
 const Mongoose = require('mongoose')
+const path = require('path')
 const app = new Koa()
 const routing = require('./routes')
 const { connectionStr } = require('./config')
@@ -32,7 +34,14 @@ app.use(Error({
     return process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
   }
 }))
-app.use(BodyParser())
+// app.use(BodyParser())
+app.use(KoaBody({
+  multipart: true, // 支持文件格式
+  formidable: {
+    uploadDir: path.join(__dirname, '/public/uploads'), // 设置文件上传目录
+    keepExtensions: true // 保留文件拓展名
+  }
+}))
 app.use(Parameter(app))
 routing(app)
 
